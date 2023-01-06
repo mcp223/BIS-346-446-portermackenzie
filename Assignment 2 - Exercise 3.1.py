4e9ceeaceff36412c1e3fe48dd2e16d0fedbf07b
Troy Adair <tra418@lehigh.edu>
2023-01-06 12:01:36 -0500

    Assignment 2 graded

--- Assignment 2 - Exercise 3.1.py
+++ Assignment 2 - Exercise 3.1.py
@@ -11,6 +11,7 @@ Created on Wed Jan  4 19:52:23 2023
 
 passes = 0 
 
+# Not sure why you're trying to oterate over student, but your script always produces 10 failures   -5
 for student in range(10):
     result = int(input('Enter result (1 = pass, 2 = fail):'))
 
@@ -20,7 +21,7 @@ for student in range(10):
         print('Incorrect result entered')
         result = int(input('Enter result (1 = pass, 2 = fail):'))
         
-    if result == 1:
+    if result == int(1):
         passes == passes + 1
         
     
