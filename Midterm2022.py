77d962ae9be811e381cbb45d71d22d790a9a5ebb
Mackenzie Porter <mcp223@lehigh.edu>
2023-01-07 14:52:43 -0500

    Midterm2022

---
+++ Midterm2022.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 12:46:59 2023
+
+@author: mackenzieporter
+"""
+
+# MIDTERM 2022
+
+# Foreign Exchange Rates
+
+us_dollar = 1
+british_pound = 0.84
+euro = 0.95
+canadian_dollar = 1.36
+
+# Define a function to convert USD amount
+
+def convert_britishpound(us_dollar):
+    return number * british_pound 
+
+def convert_euro(us_dollar):
+    return number * euro 
+
+def convert_canadiandollar(us_dollar):
+    return number * canadian_dollar
+
+# Prompt to enter USD amount
+
+number = int(input('Enter the amount of US dollars to convert: $'))
+
+# Call the function 
+
+convert_britishpound(number)
+convert_euro(number)
+convert_canadiandollar(number)
+
+# Print converted amounts with their assigned currencies 
+
+print ('British Pounds:', convert_britishpound(us_dollar), '\nEuro:', convert_euro(us_dollar), '\nCanadian Dollars:', convert_canadiandollar(us_dollar))
+
+
+
+
+
